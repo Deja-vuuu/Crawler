c09567402d3facd36608d10fd71b853a59d4617a
yangyang <yangyang@rgbvr.com>
2018-03-07 19:10:57 +0800

    test

--- app.js
+++ app.js
@@ -2,14 +2,27 @@
 
 const superagent  = require("superagent");
 const cheerio  = require('cheerio');
+const fs = require('fs');
 const reptileUrl = 'http://www.jianshu.com/';
 
 superagent.get(reptileUrl).end(function(err,res){
     if(err){
-        // throw 500
+
         console.log("出错啦~！")
     }
 
-    let $ = cheerio.load(res.text);
-    console.log($.html())
+    var  $ = cheerio.load(res.text);
+    //console.log($.html())
+  
+ 
+    fs.writeFile('message.txt', $.html(), function (err) {
+        if (err) throw err;
+        
+        console.log('写入完成');
+      });
+    
+    
+
 })
+
+//console.log(html)
